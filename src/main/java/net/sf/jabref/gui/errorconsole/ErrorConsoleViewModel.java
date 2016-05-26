cd4f820d0ef66b03b2422d01af4256adc084ae3c
Christian Bartsch <bartsch.dev@gmail.com>
2016-05-26 16:39:41 +0200

    fix automatic refresh

--- src/main/java/net/sf/jabref/gui/errorconsole/ErrorConsoleViewModel.java
+++ src/main/java/net/sf/jabref/gui/errorconsole/ErrorConsoleViewModel.java
@@ -1,34 +1,40 @@
 package net.sf.jabref.gui.errorconsole;
 
+import java.util.Random;
+
 import net.sf.jabref.Globals;
 import net.sf.jabref.logic.l10n.Localization;
 import net.sf.jabref.logic.logging.GuiAppender;
 
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.beans.property.ReadOnlyStringProperty;
 import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 public class ErrorConsoleViewModel {
 
-    private Timeline timeline;
+    //maybe make it editable as a preference?
+    private static final long REFRESH_RATE = 1000;
+
+    private Thread refreshThread = new Thread(() -> {
+        try {
+            while (true) {
+                refreshGUI();
+                Thread.sleep(REFRESH_RATE);
+            }
+        } catch (InterruptedException e) {}
+    });
+
     private final ReadOnlyStringWrapper logTabTextArea = new ReadOnlyStringWrapper();
     private final ReadOnlyStringWrapper exceptionTabTextArea = new ReadOnlyStringWrapper();
     private final ReadOnlyStringWrapper outputTabTextArea = new ReadOnlyStringWrapper();
-
-    // public FXRefresh refreshErrorGUI;
 
     @FXML
     private Button closeButton;
 
     @FXML
     private void initialize() {
-
         logTabTextArea.set(Globals.streamEavesdropper.getOutput());
         exceptionTabTextArea.set(Globals.streamEavesdropper.getErrorMessages());
         outputTabTextArea.set(GuiAppender.CACHE.get());
@@ -37,17 +43,7 @@ public class ErrorConsoleViewModel {
             exceptionTabTextArea.set(
                     Localization.lang("No exceptions have occurred."));
         }
-
-        //refreshErrorGUI.startrefreshErrorGUI();
-
-        //refreshGUI();
-
-        timeline = new Timeline(new KeyFrame(
-                Duration.millis(2500),
-                ae -> refreshGUI()));
-        timeline.setCycleCount(Animation.INDEFINITE);
-        timeline.play();
-
+        refreshThread.start();
     }
 
     public ReadOnlyStringProperty logTabTextAreaProperty() {
@@ -76,36 +72,22 @@ public class ErrorConsoleViewModel {
     }
 
     public void refreshGUI() {
-        /*Thread t = new Thread(() -> {
-            while (true) {
-                Platform.runLater(() -> System.out.println("TEST"));
-                try {
-                    Thread.sleep(2500);
-                } catch (InterruptedException ex) {
-                    break;
-                }
-            }
-        });
+        System.out.println(new Random().nextInt());  //TODO delete test dummy
 
-        t.setName("Runnable Time Updater");
-        t.setDaemon(true);
-        t.start();*/
-        System.out.println(true);
-       /* logTabTextArea.set(Globals.streamEavesdropper.getOutput());
+        logTabTextArea.set(Globals.streamEavesdropper.getOutput());
         exceptionTabTextArea.set(Globals.streamEavesdropper.getErrorMessages());
         outputTabTextArea.set(GuiAppender.CACHE.get());
 
         if (getExceptionTabTextArea().isEmpty()) {
             exceptionTabTextArea.set(
                     Localization.lang("No exceptions have occurred."));
-        }*/
+        }
     }
 
     @FXML
     private void closeErrorDialog() {
         Stage stage = (Stage) closeButton.getScene().getWindow();
-        timeline.stop();
-        //refreshErrorGUI.stoprefreshErrorGUI();
+        refreshThread.interrupt();
         stage.close();
     }
 
