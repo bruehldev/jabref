993974a28c5a12ab8f61bd7afa086988e1672287
Tobias Boceck <boceckts@gmail.com>
2016-07-18 16:02:37 +0200

    Added comments and removed built in lists button

--- src/main/java/net/sf/jabref/gui/journals/ManageJournalAbbreviationsViewModel.java
+++ src/main/java/net/sf/jabref/gui/journals/ManageJournalAbbreviationsViewModel.java
@@ -37,6 +37,13 @@ import net.sf.jabref.logic.journals.Abbreviation;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+/**
+ * This class provides a model for managing journal abbreviation lists.
+ * It provides all necessary methods to create, modify or delete journal
+ * abbreviations and files. To visualize the model one can bind the properties
+ * to ui elements.
+ *
+ */
 public class ManageJournalAbbreviationsViewModel {
 
     private final Log logger = LogFactory.getLog(ManageJournalAbbreviationsViewModel.class);
